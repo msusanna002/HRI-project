64bc788b8fd32bd7d7bb05e7bc70b2a7de079bcf
msusanna002 <susanna.mardbrant@gmail.com>
2025-12-05 18:17:05 -0700

    Initial log errors fixed

--- WeBots/controllers/TIAGO_controller/game_manager.py
+++ WeBots/controllers/TIAGO_controller/game_manager.py
@@ -84,7 +84,6 @@ class GameManager:
 
         #randomly choose one of the possible pieces
         i = random.randint(0, len(possible_pieces) - 1)
-        print("random choice index:", i)
 
         return possible_pieces[i]
 
